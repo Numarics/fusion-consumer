e8066534681307ed42b433b81662d5d4349b7a21
Mihailo <mihailo.pejic@numarics.com>
2024-12-23 16:36:58 +0100

    update ExampleService

--- src/main/java/com/company/example/ExampleService.java
+++ src/main/java/com/company/example/ExampleService.java
@@ -2,6 +2,8 @@ package com.company.example;
 
 import com.numarics.engine.fusion.client.ClientApi;
 import com.numarics.engine.fusion.client.ClientCreateResponse;
+import com.numarics.engine.fusion.document.DocumentApi;
+import com.numarics.engine.fusion.document.DocumentUploadResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,12 @@ import java.util.List;
 @Service
 public class ExampleService {
     private final ClientApi clientApi;
+    private final DocumentApi documentApi;
 
     @Autowired
-    public ExampleService(ClientApi clientApi) {
+    public ExampleService(ClientApi clientApi, DocumentApi documentApi) {
         this.clientApi = clientApi;
+        this.documentApi = documentApi;
     }
 
     public String createClient() {
@@ -25,5 +29,17 @@ public class ExampleService {
         ClientCreateResponse clientCreateResponse = clientApi.create("test", "t@t.ch", "test-client-ref-2", type, modules);
         System.out.println(clientCreateResponse);
         return clientCreateResponse.getTenantUuid();
-    };
+    }
+
+    public DocumentUploadResponse upload() {
+        String tenantUuid = "tenant-uuid-example"; // Use one from response for onboarding partner client.
+        String documentName = "example_doc.pdf";
+        Long documentSize = 12345L;
+        String contentType = "application/pdf";
+        String documentContent = "document_content"; // Document content converted to Base64
+        DocumentUploadResponse documentUploadResponse = documentApi.upload(tenantUuid,
+                documentName, documentSize, contentType, documentContent);
+        System.out.println(documentUploadResponse);
+        return documentUploadResponse;
+    }
 }
