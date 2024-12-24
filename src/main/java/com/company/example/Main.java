451949ba41ab7f8d4a9ab8e1f7865c5cdaa6d53b
Mihailo <mihailo.pejic@numarics.com>
2024-12-24 11:10:01 +0100

    update ExampleService

--- src/main/java/com/company/example/Main.java
+++ src/main/java/com/company/example/Main.java
@@ -1,6 +1,7 @@
 package com.company.example;
 
 import com.numarics.engine.fusion.client.ClientApi;
+import com.numarics.engine.fusion.client.ClientCreateResponse;
 import com.numarics.engine.fusion.document.DocumentApi;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +23,13 @@ public class Main {
   @Bean
   public CommandLineRunner run() {
     return args -> {
-      System.out.println(
-          clientApi.create("test21", "t21@t.ch", "test-client-ref-121", 2, List.of(2)));
+      ClientCreateResponse response =
+              clientApi.create("mihatest12", "mihatest21@t.ch", "test-client-ref-13434", 2, List.of(2));
+      System.out.println(response);
       FileInfo fileInfo = PDFToBase64JSON.getFileInfo("document_winner.pdf", ContentType.PDF.getValue());
       System.out.println(
           documentApi.upload(
-              "307e450e-b638-4885-8acf-889c7eae54d8",
+              response.getTenantUuid(),
               fileInfo.name(),
               fileInfo.size(),
               fileInfo.contentType(),
