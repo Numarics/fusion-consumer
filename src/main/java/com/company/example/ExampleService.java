52bfad616c99170cf62991439b3d011bbcc7566d
Milena Lalic <milena.lalic@numarics.com>
2025-01-08 10:15:39 +0100

    Add update document

--- src/main/java/com/company/example/ExampleService.java
+++ src/main/java/com/company/example/ExampleService.java
@@ -4,8 +4,14 @@ import com.numarics.engine.fusion.client.ClientApi;
 import com.numarics.engine.fusion.client.ClientCreateResponse;
 import com.numarics.engine.fusion.document.DocumentApi;
 import com.numarics.engine.fusion.document.DocumentDetailsResponse;
+import com.numarics.engine.fusion.document.DocumentUpdateRequest;
+import com.numarics.engine.fusion.document.DocumentUpdateResponse;
 import com.numarics.engine.fusion.document.DocumentUploadResponse;
 import java.util.List;
+import java.util.Set;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,13 +27,13 @@ public class ExampleService {
   }
 
   public String createClient() {
-    String clientReference = "some client reference";
-    String email = "test_email@email.ch";
-    String name = "test";
+    String clientReference = "";
+    String email = "example@email.ch";
+    String name = "ref no";
     int type = 2; // 1 is enterprise, 2 is private person
     List<Integer> modules = List.of(2); // 2 for Docubox
     ClientCreateResponse clientCreateResponse =
-        clientApi.create("test", "t@t.ch", "test-client-ref-2", type, modules);
+        clientApi.create(name, email, clientReference, type, modules);
     System.out.println(clientCreateResponse);
     return clientCreateResponse.getTenantUuid();
   }
@@ -43,6 +49,50 @@ public class ExampleService {
         documentApi.upload(tenantUuid, documentName, documentSize, contentType, documentContent);
     System.out.println(documentUploadResponse);
     return documentUploadResponse;
+  }
+
+  public DocumentUpdateResponse update() {
+    String tenantUuid =
+        "tenant-uuid-example"; // Use one from response for onboarding partner client.
+    Integer documentId = 1; // Use one from response for upload document.
+    String name = "example_doc_1.pdf";
+    Short type = 1;
+    Short status = 1;
+    List<Integer> tags = List.of(1);
+    String vendorName = "Vendor Example";
+    String vendorAddress = "Vendor Address Example";
+    String vendorTaxId = "12345";
+    String dueDate = "2024-01-01";
+    String documentDate = "2024-01-01";
+    String vatRate = "0.1";
+    String totalAmount = "100";
+    String currency = "CHF";
+    Short documentAccessType = 1;
+    List<String> emails = null;
+    Set<Short> roles = null;
+    boolean showToOperators = true;
+    DocumentUpdateResponse response =
+        documentApi.update(
+            tenantUuid,
+            documentId,
+            name,
+            type,
+            status,
+            tags,
+            vendorName,
+            vendorAddress,
+            vendorTaxId,
+            dueDate,
+            documentDate,
+            vatRate,
+            totalAmount,
+            currency,
+            documentAccessType,
+            emails,
+            roles,
+            showToOperators);
+    System.out.println(response);
+    return response;
   }
 
   public DocumentDetailsResponse getById() {
--- src/main/java/com/company/example/Main.java
+++ src/main/java/com/company/example/Main.java
@@ -4,6 +4,7 @@ import com.numarics.engine.fusion.client.ClientApi;
 import com.numarics.engine.fusion.client.ClientCreateResponse;
 import com.numarics.engine.fusion.document.DocumentApi;
 import com.numarics.engine.fusion.document.DocumentDetailsResponse;
+import com.numarics.engine.fusion.document.DocumentUpdateResponse;
 import com.numarics.engine.fusion.document.DocumentUploadResponse;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,7 @@ public class Main {
   public CommandLineRunner run() {
     return args -> {
       ClientCreateResponse response =
-          clientApi.create("mihatest12", "mihatest21@t.ch", "test-client-ref-13434", 2, List.of(2));
+          clientApi.create("noref3", "noref3@t.ch", null, 2, List.of(2));
       System.out.println(response);
 
       FileInfo fileInfo = PDFToBase64JSON.getFileInfo("income.pdf", ContentType.PDF.getValue());
@@ -42,6 +43,28 @@ public class Main {
       DocumentDetailsResponse documentDetailsResponse =
           documentApi.getById(response.getTenantUuid(), (int) uploadResponse.getId());
       System.out.println(documentDetailsResponse);
+
+      DocumentUpdateResponse documentUpdateResponse =
+          documentApi.update(
+              response.getTenantUuid(),
+              (int) uploadResponse.getId(),
+              "apple_invoice_update.pdf",
+              (short) 1,
+              (short) 5,
+              null,
+              null,
+              null,
+              null,
+              null,
+              null,
+              null,
+              null,
+              null,
+              (short) 1,
+              null,
+              null,
+              true);
+      System.out.println(documentUpdateResponse);
     };
   }
 }
